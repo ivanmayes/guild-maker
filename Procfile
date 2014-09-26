83a8825be3ba12d839a524b5d573b2218a4a1a3b
ivanmayes <ivanmayes@gmail.com>
2014-09-26 15:21:57 -0500

    Added boilerplate reorg files

--- Procfile
+++ Procfile
@@ -1,3 +1,3 @@
 admin: node admin/server.js
-app: node app.js
-gulp: gulp watch
+app: node web/app.js
+api: node api/server.js
--- web/app/config/secrets.js
+++ web/app/config/secrets.js
@@ -20,7 +20,7 @@
 
 module.exports = {
 
-  db: process.env.MONGODB|| 'mongodb://localhost:27017/nlmg',
+  db: process.env.MONGODB|| 'mongodb://localhost:27017/test',
 
   sessionSecret: process.env.SESSION_SECRET || 'Your Session Secret goes here',
 
