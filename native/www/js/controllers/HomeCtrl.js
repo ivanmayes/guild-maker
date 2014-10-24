eb0bf3c3bdf63738e6bef5e825fdd33b597d5c04
ivanmayes <ivanmayes@gmail.com>
2014-10-24 14:59:25 -0500

    Fixed navbar issues, got hiding back button working

--- native/www/js/controllers/HomeCtrl.js
+++ native/www/js/controllers/HomeCtrl.js
@@ -3,11 +3,18 @@
 define(function() {
     'use strict';
 
-    function ctrl($scope, $stateParams) {
+    function ctrl($scope, $stateParams, $ionicNavBarDelegate) {
         $scope.page_name = "Home";
+
+        setTimeout(function() {
+            // Hide back button
+            $ionicNavBarDelegate.$getByHandle('main-nav').showBackButton(false);
+            $scope.$apply();
+        }, 10);
+
     }
 
-    ctrl.$inject = ['$scope', '$stateParams'];
+    ctrl.$inject = ['$scope', '$stateParams', '$ionicNavBarDelegate'];
     return ctrl;
 
 });
--- native/www/js/controllers/controllers.js
+++ native/www/js/controllers/controllers.js
@@ -15,9 +15,9 @@ define(function(require) {
     controllers.controller('FavoriteTeamsCtrl', require('controllers/FavoriteTeamsCtrl'));
     controllers.controller('ScoresCtrl', require('controllers/ScoresCtrl'));
 
-    controllers.run(['$rootScope', function($rootScope) {
+    /*controllers.run(['$rootScope', function($rootScope) {
             $rootScope.sampleParam = "value";
-    }]);
+    }]);*/
 
     return controllers;
 
